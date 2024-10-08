cae10bb23857a132a388ac0fe3008d9d8e66a590
Lais Canabarro <laisaraujo1312@gmail.com>
2024-10-07 22:26:51 -0300

    feat: created initial structure

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../compoments/default-login-layout/default-login-layout.component';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    DefaultLoginLayoutComponent
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+
+}
