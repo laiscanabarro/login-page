d96ffc60a5d0c9846c56d5e3a34671622fb884ea
Lais Canabarro <laisaraujo1312@gmail.com>
2024-10-24 19:05:17 -0300

    feat: added page for authenticated users

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -7,15 +7,35 @@ import { tap } from 'rxjs';
   providedIn: 'root'
 })
 export class LoginService {
+  apiUrl: string = "http://localhost:8081/auth";
 
   constructor(private httpClient: HttpClient) { }
 
-  login(name: string, password: string) {
-    return this.httpClient.post<LoginResponse>("/login", { name, password }).pipe(
+  login(email: string, password: string) {
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
       })
     )
+  }
+
+  signup(name: string, email: string, password: string, passwordConfirm: string) {
+    if (password.match(passwordConfirm)) {
+      return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
+        tap((value) => {
+          sessionStorage.setItem("auth-token", value.token)
+          sessionStorage.setItem("username", value.name)
+        })
+      )
+    }
+    else {
+      return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email }).pipe(
+        tap((value) => {
+          sessionStorage.setItem("auth-token", value.token)
+          sessionStorage.setItem("username", value.name)
+        })
+      )
+    }
   }
 }
